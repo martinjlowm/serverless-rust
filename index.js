0d23861cf3546e5f434977037801b7b5df585c08
Simon Vandel Sillesen <simon.vandel@gmail.com>
2022-04-01 12:11:06 +0200

    bobo

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ class RustPlugin {
 
   getTarget_without_zig_gnu_version() {
     // strip .2.17 from `aarch64-unknown-linux-gnu.2.17`
-    const target = this.getTarget().split['.'][0]
+    const target = this.getTarget().split('.')[0]
   }
 
   localBuildArgs(funcArgs, cargoPackage, binary, profile, platform) {
